99502227d49f7a7368510252ac3b703a2cf9b4b0
Oliver.dev <oliveiramaicon16637@gmail.com>
2025-04-06 17:44:41 -0300

    Primeiro Commit

---
+++ E-QuickOrd/src/main/java/com/springboot/E_QuickOrd/EQuickOrdApplication.java
@@ -0,0 +1,13 @@
+package com.springboot.E_QuickOrd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EQuickOrdApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EQuickOrdApplication.class, args);
+	}
+
+}
---
+++ E-QuickOrd/src/test/java/com/springboot/E_QuickOrd/EQuickOrdApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springboot.E_QuickOrd;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EQuickOrdApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
